582173cc929633e7dee0c4477e1e6249ed26e259
fdll14 <shuro.fadhillah@gmail.com>
2022-09-17 19:58:04 +0700

    upload

--- listgroups/buaya.js
+++ listgroups/buaya.js
@@ -1,18 +1,21 @@
-const ads = `Garapan ini mengharuskan kalian mendafat ads.google.com
+const ads = `Garapan ini mengharuskan kalian mendaftar ads.google.com
+
 
 Berapa reward yang diberikan ? gw bayar per emailnya 40k dengan catatan mendaftaran adsnya berhasil.
 
 Pembayaran akan diberikan setelah akun yang kalian setoran aman dan ga kena penagguhan pas di pakai buyer gw.
 
-Jadi sistemnya :
-Setor email -> gw change pwnya -> buyer cek dan bayar ke gw -> kalian cair.
-
-Syarat :
-- Pastikan jangan daftar menggunakan gmail utama kalian (karena email akan saya ambil)
+Syarat Setor :
+- Pastikan email fresh dan sudah dibikin 7hari dan tidak pernah digunakan untuk pembayaran apapun. (karena email akan saya ambil)
 - Punya Laptop Atau Hp (Lebih saya rekomendasikan menggunakan laptop)
 - Ga Pernah Melakukan Pembayaran Ilegal di akun gmail tersebut (email bakal gw cek sebelum gw bayar)
 - Pastikan Verif 2 langkah mati
 - Tidak terdapat nomor hp atau google pemulihan di email tersebut.
+- Pastikan akun google ads sudah teracc selama seminggu sebelum kalian setorkan.
+
+Sistem Pembayaran :
+
+Gw bakal nampung emailnya dulu dan gw serahin ke clint gw kalo emang aman dan ga bermasalah baru gw bayar ke kalian.
 
 Tutorial :
 https://drive.google.com/drive/u/0/folders/1F_T3s0yY7e9wZAMUFk5MgVQxd1WpsLhh
@@ -26,6 +29,7 @@ const gestun = `*Gestun spaylater*
 
 instan fee 5% via scan,chat langsung ke wa.me/6285888547810
 #promotion`
+
 const garapan = `*List Garapan Buaya Cuan* :
 
 *!adsgoogle*
