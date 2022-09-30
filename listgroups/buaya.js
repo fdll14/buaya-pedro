5b8362cb5bfefcf3005dbd45a569718123502494
fdll14 <shuro.fadhillah@gmail.com>
2022-09-30 22:33:06 +0700

    upload

--- listgroups/buaya.js
+++ listgroups/buaya.js
@@ -2,31 +2,32 @@ const ads = `*Garapan Akun Ads Google*
 
 Syarat:
 - Akun gmail minimal berumur 7 hari an (jangan yang baru bikin trus langsung buat garap).
-- Punya laptop atau hp untuk garapnya.
+- Punya laptop ( lewat hp boleh asal menggunakan mode pakar )
 - Akun gmail kosongan (tidak terdapat nomor hp atau google pemulihan).
 - Pastikan verifikasi 2 langkah mati.
 - Pastikan akun ads sudah di acc selama seminggu sebelum kalian setorkan.
+- Baca dulu penjelasan dibawah sebelum garap.
 
 Tutorial :
 https://drive.google.com/drive/u/0/folders/1F_T3s0yY7e9wZAMUFk5MgVQxd1WpsLhh
 
 System pembayaran :
 
-Kalian setorin emailnya dengan sesuai syarat diatas, buyer gw coba dan liat hasilnya kalo akunnya bisa munculin *trafik* bakal dibayar.
+Kalian setorin emailnya dengan sesuai syarat diatas, buyer gw coba dan liat hasilnya kalo akunnya *bisa dapat trafik stabil* bakal dibayar.
 
-*QnA* :
+QnA :
 
-1. *Kapan pembayarannya* ? Pembayaran nunggu konfirmasi dulu dari buyer gw.
+1. Kapan pembayarannya ? Pembayaran nunggu konfirmasi dulu dari buyer gw. 
 
-2. *Bakal dibayar berapa* ? kalian bakal dibayar *100k* buat 1 orang pertama jika akun kalian bisa muncul trafik saat dipake buyer gw dan selebihnya bakal di bayar 50k.
+2. Bakal dibayar berapa ? kalian bakal dibayar 50k buat 1 orang pertama jika akun kalian bisa dapat trafik yang stabil.
 
-3. *Garapan ini bakal instan cair engga* ? Jawabannya engga karna mesti nunggu buyer gw coba akunnya terlebih dahulu.
+3. Garapan ini bakal instan cair engga ? Jawabannya engga karna mesti nunggu buyer gw coba akunnya terlebih dahulu.
 
-4. *Berapa lama kira kira biar dibayar* ? Secepatnya pokoknya tergantung buyer gw ini karna dia perlu cek akunnya satu per satu jadi butuh waktu. dan tiap pengajuan perlu nunggu di check google dulu antara bakal di acc atau engga.
+4. Berapa lama kira kira biar dibayar ? Secepatnya pokoknya tergantung buyer gw ini karna dia perlu cek akunnya satu per satu jadi butuh waktu. dan tiap pengajuan perlu nunggu di check google dulu antara bakal di acc atau engga.
 
-5. *Kenapa ko jadi ribet gini bang* ? Ya mau gimana lagi kebanyakan akunnya dicoba malah ditangguhkan semua kata buyer gw. 
+5. Kenapa ko jadi ribet gini bang ? Ya mau gimana lagi kebanyakan akunnya dicoba malah ditangguhkan semua kata buyer gw kalo ga ya ga muncul trafik stabil.
 
-6. *Kalo garap tapi akunnya ditangguhkan atau suspend gimana bang* ? dah gw omongin ke dia dan dia ngasih solusi bakal ngebayar 5k per akun yg ditangguhkan/suspend jadi ga rugi waktu banget garap ini anggap aja garapan di waktu luang.
+6. Kalo garap tapi akunnya pas dipake ditangguhkan atau suspend gimana? solusinya kalian mesti ngelakuin banding untuk akun tersebut kalo lolos banding bisa disetorin lagi dan kalo missal dipake ttp susped bakal diganti 25k ama buyer gw. kalo ga mau banding gimana ? cuma bakal dibayar 5k.
 
 Note
 *Kalo ada pertanyaan tanyakan di group jangan ngepm`
